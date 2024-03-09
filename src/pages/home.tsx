cd80a533c239b32de9432406b8aac09480648b3d
krushang patel <kp.krushang@gmail.com>
2024-03-09 10:46:22 +0530

    add task status chart

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,12 +1,17 @@
-import { VStack } from "@chakra-ui/layout"
+import { VStack, SimpleGrid } from "@chakra-ui/layout"
 
 import StatsGrid from "components/stats/stats-grid"
 import Toolbar from "components/toolbar"
+import TaskStatusChart from "components/task-status-chart"
 
 const Home = () => (
   <VStack spacing={8} w='full'>
     <StatsGrid />
     <Toolbar />
+
+    <SimpleGrid spacing={10} columns={2} w='full'>
+      <TaskStatusChart />
+    </SimpleGrid>
   </VStack>
 )
 
