e01ed34665592f4a8c487b6d16daa2c63bbc6f66
krushang patel <kp.krushang@gmail.com>
2024-03-11 13:32:54 +0530

    changes for stats grid

--- src/components/stats/stats-grid.tsx
+++ src/components/stats/stats-grid.tsx
@@ -7,7 +7,7 @@ import TaskStat from "components/stats/task-stat"
 
 const StatsGrid = () => (
   <SimpleGrid
-    spacing={10}
+    spacing={5}
     columns={{ base: 1, sm: 2, xl: 4, "2xl": 4 }}
     w='full'
   >
